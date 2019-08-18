8d266051f0a68e269cabb7526dd164e98b8207c3
Martin Wilmer <hello@martinwilmer.de>
2019-08-18 20:46:27 +0200

    Grundstruktur HTML, CSS, JS der Finder App

---
+++ prototyp/js/main.js
@@ -0,0 +1,45 @@
+console.log('[Finder App] Start');
+
+/**
+ * Cookie handling
+ */
+const visitedWebsite = Cookies.get('visited-website');
+console.log('[Finder App] Cookie gesetzt:', visitedWebsite);
+
+if (visitedWebsite === 'true') {
+  console.log('[Finder App] Finder App anzeigen');
+} else {
+  console.log('[Finder App] Startseite anzeigen');
+  Cookies.set('visited-website', 'true');
+}
+
+/**
+ * jTinder initialization
+ */
+$("#tinderslide").jTinder({
+	// dislike callback
+    onDislike: function (item) {
+      console.log('[Finder App] Button »Dislike« geklickt');
+	    // set the status text
+        $('#status').html('Dislike Post #' + (item.index()+1));
+    },
+	// like callback
+    onLike: function (item) {
+      console.log('[Finder App] Button »Like« geklickt');
+	    // set the status text
+        $('#status').html('Like Post #' + (item.index()+1));
+    },
+	animationRevertSpeed: 200,
+	animationSpeed: 400,
+	threshold: 1,
+	likeSelector: '.like',
+	dislikeSelector: '.dislike'
+});
+
+/**
+ * Set button action to trigger jTinder like & dislike.
+ */
+$('.actions .like, .actions .dislike').click(function(e){
+	e.preventDefault();
+	$("#tinderslide").jTinder($(this).attr('class'));
+});
