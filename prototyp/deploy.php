f68339e750b2e6e6692a2b7c3889a857b41b141d
Jens Wittmann <post@jens-wittmann.de>
2019-08-20 17:48:00 +0200

    deployment geht auch einfacher

--- prototyp/deploy.php
+++ prototyp/deploy.php
@@ -1,25 +1,6 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT</title>
-</head>
-<body>
+<?php
+$command = 'git pull git@github.com:jenswittmann/zeigwasdumachst.git';
+exec($command.' 2>&1', $tmp, $return_code);
+?>
+$ <?php echo $command; ?>: <?php echo implode("\n", $tmp); ?>
 
-	<?php
-	$commands = array(
-		'git pull git@github.com:jenswittmann/zeigwasdumachst.git',
-	);
-		
-	foreach($commands AS $command){
-		$tmp = shell_exec($command);	
-	?>
-	
-		$ <?php echo $command; ?>: <?php echo htmlentities(trim($tmp)); ?><br>
-		
-	<?php
-	}
-	?>
-	
-</body>
-</html>
