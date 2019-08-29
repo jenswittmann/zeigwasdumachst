c8119105c7fd0cbb35abd7be6bf974a9165b3e19
Jens Wittmann <post@jens-wittmann.de>
2019-08-29 11:14:45 +0200

    Tachyons Framework lokal nutzen

--- prototyp/index.php
+++ prototyp/index.php
@@ -4,7 +4,7 @@
 	    <title>Findr APP</title>
 	    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
+		<link rel="stylesheet" type="text/css" href="css/tachyons.css">
 	    <link rel="stylesheet" type="text/css" href="css/jTinder.css?v2">
 	    <link rel="stylesheet" type="text/css" href="css/main.css?v2">
 	</head>
